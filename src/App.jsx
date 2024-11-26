ec7940ab1b8b90b395f2e4f318336160f8b4dbc6
luketucich <ltucich@gmail.com>
2024-11-26 02:52:13 -0500

    feat: Clicking a card will check if it has already been clicked, and will update score accordingly. Furthermore, the deck will shuffle on each successful click, but will reset on a repeat click.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import Cards from "./Cards";
 
 export default function App() {
+  const [score, setScore] = useState(0);
+  // const [highScore, setHighScore] = useState(0);
+
   return (
     <>
-      <Cards />
+      <Cards score={score} setScore={setScore} />
+      <div className="flex justify-center p-10">
+        <p className="text-2xl text-white font-bold">Score: {score}</p>
+      </div>
     </>
   );
 }
--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,23 +1,60 @@
 import { useEffect, useState } from "react";
 
-export default function Cards() {
+export default function Cards({ score, setScore }) {
   const [deck, setDeck] = useState(null);
+
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  const handleCardClick = (card) => {
+    // Update score and clicked cards
+    setDeck((prev) => {
+      if (prev.clickedCards.includes(card)) {
+        setScore(0);
+        alert("You lose! Try again.");
+        fetchDeck();
+        return prev;
+      } else {
+        setScore(score + 1);
+        return {
+          ...prev,
+          cards: shuffle(prev.cards),
+          clickedCards: [...prev.clickedCards, card],
+        };
+      }
+    });
+  };
+
+  const fetchDeck = () => {
+    fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=52")
+      .then((response) => response.json())
+      .then((data) =>
+        setDeck({
+          cards: data.cards,
+          clickedCards: [],
+          id: crypto.randomUUID(),
+        })
+      )
+      .catch((error) => console.error(error));
+  };
 
   // Fetch card deck from API
   useEffect(() => {
-    fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=5")
-      .then((response) => response.json())
-      .then((data) => setDeck(data.cards))
-      .catch((error) => console.error(error));
+    fetchDeck();
   }, []);
 
   // Display cards
   return (
-    <div className="flex flex-col gap-5 justify-center items-center p-10">
+    <div className="flex flex-wrap gap-16 justify-center p-10">
       {deck &&
-        deck.map((card) => (
+        deck.cards.slice(0, 5).map((card) => (
           <div
-            key={crypto.randomUUID()}
+            key={card.code}
             className="flex flex-col 
                     items-center
                     hover:scale-110
@@ -25,12 +62,12 @@ export default function Cards() {
                     hover:skew-x-1
                     hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.75)]
                     transition-transform duration-500 ease-out transform-gpu
-                    w-max
-                    h-max
                     "
+            // Add card to used pile
+            onClick={() => handleCardClick(card)}
           >
-            <img src={card.image} alt={card.code} className="w-16 h-auto " />
-            <p className="text-white font-bold">
+            <img src={card.image} alt={card.code} className="w-20 h-auto " />
+            <p className="text-white font-bold w-20 text-center">
               {card.value.charAt(0) +
                 card.value.substring(1).toLowerCase() +
                 " of " +
